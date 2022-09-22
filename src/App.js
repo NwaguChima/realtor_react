adcf76a6937e8670d8cbe865864601fdffe4fee5
NwaguChima <nwaguchima48@gmail.com>
2022-09-22 23:31:25 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1>React App</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
