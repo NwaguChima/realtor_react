25bcf23c59f4803f4fcd836caea9386bf5bd97bc
NwaguChima <nwaguchima48@gmail.com>
2022-09-23 18:28:59 +0100

    added sigin header

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+    </section>
+  );
 };
 
 export default SignIn;
