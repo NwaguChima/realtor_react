00dd60689ebb24d822e923411bd52893c61c0b29
NwaguChima <nwaguchima48@gmail.com>
2022-09-23 19:12:02 +0100

    added form states

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  function onChange(e) {
+    setFormData((prevState) => {
+      return { ...prevState, [e.target.id]: e.target.value };
+    });
+  }
+
+  console.log(formData);
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
-      <div>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
             src="https://images.unsplash.com/photo-1553991562-9f24b119ff51?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1339&q=80"
@@ -12,9 +27,16 @@ const SignIn = () => {
             className="w-full rounded-2xl object-cover"
           />
         </div>
-        <div className="w-full md:w-[67%] lg:w-[40%]">
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
-            <input className="w-full" type="text" />
+            <input
+              className="w-full"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
+            />
           </form>
         </div>
       </div>
