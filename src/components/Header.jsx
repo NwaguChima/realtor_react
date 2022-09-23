93b45deb38528437623255a84943f1a35ccdb751
NwaguChima <nwaguchima48@gmail.com>
2022-09-23 18:06:57 +0100

    syled list items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
+            alt="logo"
+            className="h-5 cursor-pointer"
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li>Home</li>
+            <li>Offers</li>
+            <li>Sign in</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Header;
