06f93234eea05f6a2273633110d24969e198be8e
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 10:41:54 +0100

    completed profile form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,14 +10,21 @@ const Profile = () => {
 
   return (
     <>
-      <section>
+      <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
-        <div>
+        <div className="w-full md:w-[50%] mt-6 px-3">
           <form>
             <input
               type="text"
               id="name"
               value={name}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+            />
+            <input
+              type="email"
+              id="email"
+              value={email}
               disabled
               className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
             />
