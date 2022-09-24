91ad72b2e11ea5f6886dc283cf98eacf43976f48
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 10:38:47 +0100

    added profile name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [formData, setFormData] = React.useState({
+    name: "Chumzy",
+    email: "chumzy@gmail.com",
+  });
+
+  const { name, email } = formData;
+
+  return (
+    <>
+      <section>
+        <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+        <div>
+          <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled
+              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6"
+            />
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Profile;
