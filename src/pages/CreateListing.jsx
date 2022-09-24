de863c64d0b0a3689606297e9e504afe840d9c6b
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 21:57:35 +0100

    added submit button

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -272,6 +272,12 @@ function CreateListing() {
             className="w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
           />
         </div>
+        <button
+          type="submit"
+          className="mb-6 w-full px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg active:bg-blue-700 transition duration-200 ease-in-out"
+        >
+          Create Listing
+        </button>
       </form>
     </main>
   );
