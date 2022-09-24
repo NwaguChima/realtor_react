e9cd12140a1830bcc44fc8342412a70cce1fd45c
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 22:46:46 +0100

    getting location from google api

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -67,7 +67,7 @@ function CreateListing() {
     }
   }
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     setLoading(true);
 
@@ -81,6 +81,25 @@ function CreateListing() {
       setLoading(false);
       toast.error("Maximum 6 images allowed");
       return;
+    }
+
+    let geoLocation = {};
+    let location;
+    if (geoLocationEnabled) {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+      );
+      const data = await response.json();
+      geoLocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geoLocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+      location = data.status === "ZERO_RESULTS" && undefined;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a correct address");
+        return;
+      }
     }
   }
 
