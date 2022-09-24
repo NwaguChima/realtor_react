281edde60cc302269174f2b1cb7dccd7c803fc89
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 10:05:19 +0100

    auth with google provider

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,8 +1,27 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
+import { toast } from "react-toastify";
 
 const OAuth = () => {
+  async function handleGoogleClick() {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+    } catch (error) {
+      console.log(error);
+      toast.error("Could not sign in with Google");
+    }
+  }
+
   return (
-    <button className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded">
+    <button
+      type="button"
+      onClick={handleGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
       <FcGoogle className="text-2xl bg-white rounded-full mr-2" />
       Continue with Google
     </button>
