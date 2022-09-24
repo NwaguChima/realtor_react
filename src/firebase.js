984ec2f24b47141493873cbb3a8d998f7498d1b2
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 09:22:40 +0100

    added firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADjeWSz6jKCsEsg9mOunmsB9s_rQrfxpc",
+  authDomain: "realtor-react.firebaseapp.com",
+  projectId: "realtor-react",
+  storageBucket: "realtor-react.appspot.com",
+  messagingSenderId: "949610034846",
+  appId: "1:949610034846:web:a8efc716e28556b69b2d8f",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
