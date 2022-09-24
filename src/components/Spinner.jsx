1f524524708c0b1f65a15a7a3fb48f20aff22497
NwaguChima <nwaguchima48@gmail.com>
2022-09-24 11:47:46 +0100

    created full spinner with svg

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import spinner from "../assets/svg/spinner.svg";
 
 const Spinner = () => {
-  return <div>Spinner</div>;
+  return (
+    <div>
+      <div>
+        <img src={spinner} alt="loading" className="h-24" />
+      </div>
+    </div>
+  );
 };
 
 export default Spinner;
