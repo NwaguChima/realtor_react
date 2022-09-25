a138317da63dac6c16e43aba014cec959cfdc090
NwaguChima <nwaguchima48@gmail.com>
2022-09-25 01:47:29 +0100

    added moment

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
 
 const ListingItem = ({ listing, id }) => {
-  return <div>{listing.name}</div>;
+  return (
+    <li>
+      <Link to={`/category/${listing.type}/${id}`}>
+        <img src={listing.imgUrls[0]} alt="listed home" />
+        <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
+      </Link>
+    </li>
+  );
 };
 
 export default ListingItem;
