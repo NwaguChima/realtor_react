6fb5358bdfca4a5a0c28cfb969a00eea01aa02d9
NwaguChima <nwaguchima48@gmail.com>
2022-09-25 09:54:22 +0100

    added swiper

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { doc, getDoc } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "Swiper/css/bundle";
 
 const Listing = () => {
-  return <div>Listing</div>;
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { listingId } = useParams();
+  SwiperCore.use(Autoplay, Navigation, Pagination);
+
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        console.log("listing", docSnap.data());
+        setLoading(false);
+      }
+    }
+
+    fetchListing();
+  }, [listingId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return <div>{listing.name}</div>;
 };
 
 export default Listing;
