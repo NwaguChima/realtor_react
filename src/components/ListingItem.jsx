43c143f4a865b091794fd97cbeaedbda7b311e3c
NwaguChima <nwaguchima48@gmail.com>
2022-09-25 01:55:09 +0100

    rendered complete listing

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Moment from "react-moment";
 import { Link } from "react-router-dom";
+import { MdLocationOn } from "react-icons/md";
 
 const ListingItem = ({ listing, id }) => {
   return (
@@ -8,6 +9,38 @@ const ListingItem = ({ listing, id }) => {
       <Link to={`/category/${listing.type}/${id}`}>
         <img src={listing.imgUrls[0]} alt="listed home" />
         <Moment fromNow>{listing.timestamp?.toDate()}</Moment>
+        <div>
+          <div className="">
+            <MdLocationOn />
+            <p>{listing.address}</p>
+          </div>
+          <p>{listing.name}</p>
+          <p>
+            $
+            {listing.offer
+              ? listing.discountedPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+              : listing.regularPrice
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            {listing.type === "rent" && "/ month"}
+          </p>
+          <div className="">
+            <div className="">
+              <p>
+                {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+              </p>
+            </div>
+            <div className="">
+              <p>
+                {listing.bathrooms > 1
+                  ? `${listing.bathrooms} Baths`
+                  : "1 Bath"}
+              </p>
+            </div>
+          </div>
+        </div>
       </Link>
     </li>
   );
