acc54e67bc8fc873840462df7ae81e7b8b71a3a8
NwaguChima <nwaguchima48@gmail.com>
2022-09-27 00:24:28 +0100

    setup fetch listing function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Slider from "../components/Slider";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    async function fetchListings() {
+      const listings = await fetch(
+        "https://us-central1-rental-clone-1a6f7.cloudfunctions.net/api/listings"
+      );
+      const listingsJson = await listings.json();
+      console.log(listingsJson);
+    }
+
+    fetchListings();
+  }, []);
+
+  return (
+    <div>
+      <Slider />
+    </div>
+  );
 };
 
 export default Home;
