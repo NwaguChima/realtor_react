db20fdbdf711de22c0ca834d05f19bf088aada25
NwaguChima <nwaguchima48@gmail.com>
2022-09-27 01:35:37 +0100

    rendered slideer

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,10 +2,21 @@ import { collection, getDocs, limit, orderBy, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import Spinner from "../components/Spinner";
 import { db } from "../firebase";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
+import { useNavigate } from "react-router-dom";
 
 const Slider = () => {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
+  SwiperCore.use([Autoplay, Navigation, Pagination]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function fetchListings() {
@@ -34,7 +45,35 @@ const Slider = () => {
   if (listings.length === 0) {
     return <></>;
   }
-  return <div>Slider</div>;
+  return (
+    listings && (
+      <>
+        <Swiper
+          slidesPerView={1}
+          navigation
+          pagination={{ type: "progressbar" }}
+          effect="fade"
+          modules={[EffectFade]}
+          autoplay={{ delay: 3000 }}
+        >
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                className="w-full h-[300px] overflow-hidden"
+                style={{
+                  background: `url(${data.imgUrls[0]}) center, no-repeat`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  );
 };
 
 export default Slider;
