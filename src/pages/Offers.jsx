939b4c2f60d712eded29a6e5d6824fdfb06b8d0a
NwaguChima <nwaguchima48@gmail.com>
2022-09-27 18:30:12 +0100

    completed offeers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -4,6 +4,7 @@ import {
   limit,
   orderBy,
   query,
+  startAfter,
   where,
 } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
@@ -15,6 +16,7 @@ import { db } from "../firebase";
 const Offers = () => {
   const [listings, setListings] = useState();
   const [loading, setLoading] = useState(true);
+  const [lastFetchedListing, setLastFetchedListing] = useState(null);
 
   useEffect(() => {
     async function fetchListings() {
@@ -29,6 +31,9 @@ const Offers = () => {
         );
 
         const querySnap = await getDocs(q);
+        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        setLastFetchedListing(lastVisible);
+
         const listings = [];
 
         querySnap.forEach((doc) => {
@@ -49,9 +54,44 @@ const Offers = () => {
     fetchListings();
   }, []);
 
+  async function handleFetchMoreListings() {
+    try {
+      const listingsRef = collection(db, "listings");
+
+      const q = query(
+        listingsRef,
+        where("offer", "==", true),
+        orderBy("timestamp", "desc"),
+        startAfter(lastFetchedListing),
+        limit(4)
+      );
+
+      const querySnap = await getDocs(q);
+      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+      setLastFetchedListing(lastVisible);
+
+      const listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings((prevListings) => {
+        return [...prevListings, ...listings];
+      });
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to fetch listings");
+    }
+  }
+
   return (
     <div className="max-w-6xl mx-auto p-3">
-      <h1 className="text-3xl text-center mt-6 font-bold">Offers</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
 
       {loading ? (
         <Spinner />
@@ -68,6 +108,16 @@ const Offers = () => {
               ))}
             </ul>
           </main>
+          {lastFetchedListing && (
+            <div className="flex justify-center items-center">
+              <button
+                className="bg-white px-3 py-1.5 text-gray-700 border-gray-300 mb-6 mt-6 hover:border-slate-600 rounded transition duration-150 ease-in-out"
+                onClick={handleFetchMoreListings}
+              >
+                Load More
+              </button>
+            </div>
+          )}
         </>
       ) : (
         <p>There are no current offers</p>
